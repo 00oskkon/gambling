ed36ed3ea49d804d1fd4e0a6d8ac19bdb0a0ca2b
00oskkon <00oskkon@protonmail.com>
2025-05-03 12:04:44 +0200

    You can choose the min and max numbers with a dialogue box

--- gambling.sh
+++ gambling.sh
@@ -11,21 +11,21 @@ if ! [ -x "$(command -v /usr/bin/zenity)" ]; then
 fi
 
 playAgain=0
-min=1
-max=100
 guess=0
-response=""
 
 while [ "$playAgain" == 0 ]
 do
+	min=$(zenity --entry --text="Enter your min number:") || exit 1
+	max=$(zenity --entry --text="Enter your max number:") || exit 1
+
 	number=$(($RANDOM % ($max - $min + 1) + $min))
  	score=$(($max+1))
 	#echo "$number"
-	
+
 	while [ "$guess" != "$number" ]
 	do
 		guess=$(zenity --entry --title="Gambling!" --text="Guess a number between $min - $max:") || exit 1
-		
+
 		if [[ "$guess" > "$number" ]]
 		then
 			#echo "$guess"
@@ -35,10 +35,10 @@ do
 			#echo "$guess"
 			zenity --error --text="$guess is too low!"
 		fi
-		
+
 		((score--))
 	done
-	
+
 	zenity --info --text="Number: $number\nYou WIN!\nScore: $score"
 	zenity --question --text="Play again?"
 	playAgain=$?
